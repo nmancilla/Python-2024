d29a749c4f93d45d7b47ed4296dc8abd70a9bb1e
Nicolas Mancilla <70174680+nmancilla@users.noreply.github.com>
2024-03-22 20:54:57 -0300

    primer commit

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+var nombre = document.getElementById("nombre");
+var apellido = document.getElementById("apellido");
+var profesion = document.getElementById("profesion");
+var salario = document.getElementById("salario");
+
+nombre.innerHTML = prompt("Ingrese un nombre");
+apellido.innerHTML = prompt("Ingrese un apellido");
+profesion.innerHTML = prompt("Ingrese un profesion");
+salario.innerHTML = prompt("Ingrese un salario");
